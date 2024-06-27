2aa67af5bdce03b8bf3558d6f5d194b94b391417
Xiao Zhong <zhongxiao2017@gmail.com>
2024-06-27 23:53:52 +1000

    change iframe  to div

--- module-script.js
+++ module-script.js
@@ -1,26 +1,25 @@
 import currentTime from './modules/top.js'
 import { weatherDisplay, amazonDisplay } from './modules/iconContentDisplay.js'
-import { loadIframeContent } from './modules/messageDisplay.js';
+// import { loadIframeContent } from './modules/messageDisplay.js';
 
 
-export const messageFrame = document.getElementById('message-frame')
-
-
-const timeDisplay = document.querySelector('.time')
 
 export const contentFrame=document.querySelector('#content-frame')
 
-const weatherIcon = document.querySelector('.weatherIcon')
-const amazonIcon = document.querySelector('.amazonIcon')
-const messageIcon=document.querySelector('.messageIcon')
-
+const timeDisplay = document.querySelector('.time');
+const weatherIcon = document.querySelector('.weatherIcon');
+const amazonIcon = document.querySelector('.amazonIcon');
+const messageIcon = document.querySelector('.messageIcon');
+const backArrow = document.querySelector('.back-symbol');
 const mainSection = document.querySelector('.main-section');
-const backArrow=document.querySelector('.back-symbol')
-const contentDisplay=document.querySelector('.content-display')
-const messageDisplay = document.querySelector('.message-display')
+const contentDisplay = document.querySelector('.content-display');
+const messageDisplaySection = document.querySelector('.message-display');
+const messageList = document.querySelector('.message-ul');
+const messageDisplay = document.getElementById('message-data-display');
 
+let isViewingMessageList = false;
 
-
+// show current time on the navbar
 timeDisplay.textContent = currentTime()
 
 
@@ -32,20 +31,6 @@ weatherIcon.addEventListener('click', (e) => {
   contentDisplay.classList.remove('noDisplay')
 })
 
-// backArrow.addEventListener('click', (e) => {
-//   e.preventDefault()
-//   console.log('back');
-//   mainSection.classList.remove('noDisplay')
-//   contentDisplay.classList.add('noDisplay')
-//   messageDisplay.classList.add('noDisplay')
-// })
-
-
-
-
-
-
-
 // below doesn't work 
 // chrome-error://chromewebdata/:1 Refused to display 'https://www.ebay.com.au/' in a frame because it set 'X-Frame-Options' to 'sameorigin'. 
 
@@ -56,13 +41,51 @@ amazonIcon.addEventListener('click', (e) => {
   contentDisplay.classList.remove('noDisplay')
 })
 
-messageIcon.addEventListener('click', (e) => {
-  e.preventDefault()
-  messageDisplay.classList.remove('noDisplay')
-  mainSection.classList.add('noDisplay')
-  console.log('yyyy');
-})
 
-document.addEventListener('DOMContentLoaded', () => {
-  loadIframeContent();
+
+
+// Event listener for messageIcon
+messageIcon.addEventListener('click', (e) => {
+  e.preventDefault();
+  mainSection.classList.add('noDisplay');
+  contentDisplay.classList.add('noDisplay');
+  messageDisplaySection.classList.remove('noDisplay');
+  messageList.classList.remove('hidden');
+  messageDisplay.classList.add('hidden');
+  isViewingMessageList = true;
+});
+
+// Event listeners for message list items
+const messageItems = document.querySelectorAll('.message-li');
+messageItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const messageContent = item.getAttribute('data-message');
+    messageDisplay.innerHTML = `<p>${messageContent}</p>`;
+    messageList.classList.add('hidden');
+    messageDisplay.classList.remove('hidden');
+  });
+});
+
+// Event listener for backArrow
+backArrow.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (isViewingMessageList) {
+    if (messageDisplay.classList.contains('hidden')) {
+      // If viewing message list, go back to home page
+      mainSection.classList.remove('noDisplay');
+      contentDisplay.classList.add('noDisplay');
+      messageDisplaySection.classList.add('noDisplay');
+      messageList.classList.add('hidden');
+      isViewingMessageList = false;
+    } else {
+      // If viewing message details, go back to message list
+      messageDisplay.classList.add('hidden');
+      messageList.classList.remove('hidden');
+    }
+  } else {
+    // Default behavior if not viewing message list or details
+    mainSection.classList.remove('noDisplay');
+    contentDisplay.classList.add('noDisplay');
+    messageDisplaySection.classList.add('noDisplay');
+  }
 });
